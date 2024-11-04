d2fb53fcdd43bdd6385283cc634ba5ad45833f02
Alastair <alastair_mcg@outlook.com>
2024-11-04 14:42:25 +0000

    initial graphs and code

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ data_dir="" #replace with your own address for data
 v = VERIS(json_dir=data_dir)
 print (v.schema_url)
 veris_df = v.json_to_df(verbose=True)
-date_ = veris_df['timeline.month'].value_counts()
-date_ = date_[date_ == 6]
-print(date_)
+
+industryCounts=veris_df['victim.industry'].value_counts()
+
+industryCounts = industryCounts[industryCounts > 20]
+
+plt.figure(figsize=(15, 6))
+industryCounts.plot(kind='bar')
+
+
+
+plt.title('Number of Incidents by Industry')
+plt.xlabel('Industry')
+plt.ylabel('Number of Incidents')
+plt.xticks(rotation=90)
+plt.show()
+
 
 
 
